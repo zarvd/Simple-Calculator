5b6ec9550ac32ed16d91919bb75d8eaba2ca18b0
GallonChoi <404842441@qq.com>
2015-07-19 15:16:31 +0000

    Minus test

--- test.cpp
+++ test.cpp
@@ -6,6 +6,7 @@ using namespace calculator;
 
 
 void testAdd() {
+    std::cout << "===================" << std::endl;
     std::cout << "TEST Add:" << std::endl;
     BigInt A("1"), B("9999");
     std::cout << "A = " << A.toStr() << std::endl;
@@ -14,14 +15,26 @@ void testAdd() {
 }
 
 void testMinus() {
+    std::cout << "===================" << std::endl;
     std::cout << "TEST Minus:" << std::endl;
     BigInt A("2"), B("1");
+    std::cout << "A = " << A.toStr() << std::endl;
+    std::cout << "B = " << B.toStr() << std::endl;
+    std::cout << "A - B = " << (A - B).toStr() << std::endl;
+    A = "1";
+    B = "2";
+    std::cout << "A = " << A.toStr() << std::endl;
+    std::cout << "B = " << B.toStr() << std::endl;
+    std::cout << "A - B = " << (A - B).toStr() << std::endl;
+    A = "100000";
+    B = "1";
     std::cout << "A = " << A.toStr() << std::endl;
     std::cout << "B = " << B.toStr() << std::endl;
     std::cout << "A - B = " << (A - B).toStr() << std::endl;
 }
 
 void testCompare() {
+    std::cout << "===================" << std::endl;
     std::cout << "TEST Compare:" << std::endl;
     BigInt A("-1"), B("-1");
     A = "5";
